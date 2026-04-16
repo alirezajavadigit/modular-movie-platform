5b39941ba716b7db00e2bffe4725fa627a9e7352
alireza javadi <alirezajavadigit@gmail.com>
2026-04-16 05:06:14 +0330

    refactor(movie): bind FileUploadService and fix translation namespace

--- Modules/Movie/app/Providers/MovieServiceProvider.php
+++ Modules/Movie/app/Providers/MovieServiceProvider.php
@@ -5,11 +5,13 @@ namespace Modules\Movie\Providers;
 use Illuminate\Support\Facades\Route;
 use Modules\Movie\Contracts\EpisodeRepositoryInterface;
 use Modules\Movie\Contracts\EpisodeServiceInterface;
+use Modules\Movie\Contracts\FileUploadServiceInterface;
 use Modules\Movie\Contracts\MovieRepositoryInterface;
 use Modules\Movie\Contracts\MovieServiceInterface;
 use Modules\Movie\Repositories\EpisodeRepository;
 use Modules\Movie\Repositories\MovieRepository;
 use Modules\Movie\Services\EpisodeService;
+use Modules\Movie\Services\FileUploadService;
 use Modules\Movie\Services\MovieService;
 use Nwidart\Modules\Support\ModuleServiceProvider;
 
@@ -37,13 +39,12 @@ class MovieServiceProvider extends ModuleServiceProvider
         $this->app->bind(MovieServiceInterface::class, MovieService::class);
         $this->app->bind(EpisodeRepositoryInterface::class, EpisodeRepository::class);
         $this->app->bind(EpisodeServiceInterface::class, EpisodeService::class);
+        $this->app->bind(FileUploadServiceInterface::class, FileUploadService::class);
     }
 
     public function boot(): void
     {
-        parent::boot();
-
-        $this->loadTranslationsFrom(module_path('Movie', 'resources/lang'), 'movie-module');
+        $this->loadTranslationsFrom(module_path('Movie', 'resources/lang'), 'movie');
 
         Route::middleware('api')
             ->group(module_path('Movie', '/routes/api.php'));
