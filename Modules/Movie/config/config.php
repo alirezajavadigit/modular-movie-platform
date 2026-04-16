df2d8e4c9c264d307ffaeecb89718567d3248c90
alireza javadi <alirezajavadigit@gmail.com>
2026-04-16 05:06:24 +0330

    feat(movie): add upload configuration for movie and episode posters

--- Modules/Movie/config/config.php
+++ Modules/Movie/config/config.php
@@ -1,3 +1,12 @@
 <?php
 
-return [];
+return [
+    'upload' => [
+        'disk' => env('MOVIE_UPLOAD_DISK', 'public'),
+
+        'directories' => [
+            'movie_posters'  => 'movies/posters',
+            'episode_posters' => 'episodes/posters',
+        ],
+    ],
+];
