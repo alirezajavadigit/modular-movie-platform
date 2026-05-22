e5e928b5007e6814616a915b8e0a67409cf1cfde
alireza javadi <alirezajavadigit@gmail.com>
2026-05-22 19:12:23 +0330

    feat(payment): define contracts and DTOs

--- Modules/Payment/app/Contracts/GatewayInterface.php
+++ Modules/Payment/app/Contracts/GatewayInterface.php
@@ -3,10 +3,11 @@
 namespace Modules\Payment\Contracts;
 
 use Modules\Payment\DTOs\CreatePaymentDTO;
+use Modules\Payment\DTOs\PurchaseResultDTO;
 
 interface GatewayInterface
 {
-    public function purchase(CreatePaymentDTO $paymentDTO): string;
+    public function purchase(CreatePaymentDTO $paymentDTO): PurchaseResultDTO;
 
-    public function verify(string $transactionId): bool;
+    public function verify(string $transactionId, float $amount = 0.0): bool;
 }
